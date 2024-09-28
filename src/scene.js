a446c64477605fb4bbcff0cf3a43dbba639c2b88
itsmeeee0607 <amollychoudhary@gmail.com>
2024-09-28 17:53:52 +0530

    added spaces for buildings

--- src/scene.js
+++ src/scene.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { createCamera } from './camera.js';
+import { createCity } from './city.js';
 
 export function createScene(){
     //Initial scene setup
@@ -12,10 +13,54 @@ export function createScene(){
     renderer.setSize(gameWindow.offsetWidth, gameWindow.offsetHeight);
     gameWindow.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    let meshes = [];
+
+    function initialize(city){
+        scene.clear();
+        meshes = [];
+        for(let x = 0; x < city.size; x++){
+            const column = [];
+            for(let y = 0; y < city.size; y++){
+                // Grass Geometry
+                const geometry = new THREE.BoxGeometry(1, 1, 1);
+                const material = new THREE.MeshLambertMaterial({ color: 0x00aa00 })
+                const mesh = new THREE.Mesh(geometry, material);
+                mesh.position.set(x, -0.5, y);
+                scene.add(mesh);
+                column.push(mesh);
+
+                // Building Geometry
+                const tile = city.data[x][y];
+
+                if (tile.building === 'building') {
+                    const buildingGeometry = new THREE.BoxGeometry(1, 1, 1);
+                    const buildingMaterial = new THREE.MeshLambertMaterial({ color: 0x777777 })
+                    const buildingMesh = new THREE.Mesh(buildingGeometry, buildingMaterial);
+                    buildingMesh.position.set(x, 0, y);
+                    scene.add(buildingMesh);
+                    column.push(buildingMesh);
+                }   
+            }
+            meshes.push(column);
+        }
+
+        setupLights();
+    }
+
+    function setupLights() {
+        const lights = [
+            new THREE.AmbientLight(0xffffff, 0.2),
+            new THREE.DirectionalLight(0xffffff, 0.3),
+            new THREE.DirectionalLight(0xffffff, 0.3),
+            new THREE.DirectionalLight(0xffffff, 0.3)
+        ];
+
+        lights[1].position.set(0, 1, 0);
+        lights[2].position.set(1, 1, 0);
+        lights[3].position.set(0, 1, 1);
+
+        scene.add(...lights);
+    }
 
     function draw() {
         renderer.render(scene, camera.camera);
@@ -42,6 +87,7 @@ export function createScene(){
     }
 
     return{
+        initialize,
         start,
         stop,
         onMouseDown,
